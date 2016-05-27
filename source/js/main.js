2b9effc259463b3c012b591a1452ae39819bb929
Sebastian Ekström <seb.ekstrom@gmail.com>
2016-05-27 21:36:45 +0200

    Update copy

--- source/js/main.js
+++ source/js/main.js
@@ -2,19 +2,11 @@
 'use strict';
 
 var $ = require('jQuery'),
-    Modernizr = require('modernizr');
+    Unfocused = require('unfocused');
 
 window.jQuery = window.$ = $;
 
 $(document).ready(function() {
-    var Example             = require('./components/example'),
-        MediaQueryListener  = require('./components/mediaquery-listener'),
-        example,
-        mediaquerylistener;
-
-    $('.element').each(function(i, elem) {
-        example = new Example({element: elem});
-    });
-
-    mediaquerylistener = new MediaQueryListener();
+    var unfocused = new Unfocused();
+    unfocused.init("I'm not focused!");
 });
